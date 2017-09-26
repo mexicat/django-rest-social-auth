a29151777be1d7c15c8dc70859ec72703e390e15
giacomo <mexicat@users.noreply.github.com>
2017-09-26 15:31:17 +0200

    whitelisting fields in serializer instead of blacklisting

--- rest_social_auth/serializers.py
+++ rest_social_auth/serializers.py
@@ -26,8 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        exclude = ('is_staff', 'is_active', 'date_joined', 'password',
-                   'last_login', 'user_permissions', 'groups', 'is_superuser',)
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 
 class TokenSerializer(serializers.Serializer):
